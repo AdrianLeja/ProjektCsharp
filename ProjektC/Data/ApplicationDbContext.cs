4e45421344fdb00cb49ca546a5dcb6bd02874da5
Adrian Leja <w69985@student.wsiz.edu.pl>
2025-01-27 23:52:37 +0100

    Dodaj pliki projektów.

---
+++ ProjektC/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProjektC.Models;
+
+namespace ProjektC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Zwierze> Zwierze { get; set; } = default!;
+        public DbSet<Adopcja> Adopcja { get; set; } = default!;
+        public DbSet<Lokacja> Lokacja { get; set; } = default!;
+        public DbSet<Pracownik> Pracownik { get; set; } = default!;
+        public DbSet<Uzytkownik> Uzytkownik { get; set; } = default!;
+
+       
+        //wywolanie procedury
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            int result = await base.SaveChangesAsync(cancellationToken);
+
+            await Database.ExecuteSqlRawAsync("EXEC AktualizujStatusAdopcji", cancellationToken);
+
+            return result;
+        }
+    }
+}
