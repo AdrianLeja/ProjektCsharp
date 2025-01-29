8febfd327b87b728b6c8f5e292960aeeb6f995ce
Adrian Leja <w69985@student.wsiz.edu.pl>
2025-01-29 13:54:35 +0100

    29.01

--- ProjektC/Controllers/AdopcjasController.cs
+++ ProjektC/Controllers/AdopcjasController.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -23,30 +22,29 @@ namespace ProjektC.Controllers
         // GET: Adopcjas
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Adopcja.Include(a => a.IdPracownikaNavigation).Include(a => a.IdUzytkownikaNavigation).Include(a => a.IdZwierzeciaNavigation);
+            var applicationDbContext = _context.Adopcja
+                .Include(a => a.IdPracownikaNavigation)
+                .Include(a => a.IdUzytkownikaNavigation)
+                .Include(a => a.IdZwierzeciaNavigation);
             return View(await applicationDbContext.ToListAsync());
         }
 
         // GET: Adopcjas/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var adopcja = await _context.Adopcja
                 .Include(a => a.IdPracownikaNavigation)
                 .Include(a => a.IdUzytkownikaNavigation)
                 .Include(a => a.IdZwierzeciaNavigation)
                 .FirstOrDefaultAsync(m => m.IdAdopcji == id);
-            if (adopcja == null)
-            {
-                return NotFound();
-            }
+
+            if (adopcja == null) return NotFound();
 
             return View(adopcja);
         }
+
         [Authorize(Roles = "Admin")]
         // GET: Adopcjas/Create
         public IActionResult Create()
@@ -58,49 +56,47 @@ namespace ProjektC.Controllers
         }
 
         // POST: Adopcjas/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("IdAdopcji,IdZwierzecia,IdUzytkownika,IdPracownika,StatusAdopcji,DataRozpoczeciaAdopcji,DataZakonczeniaAdopcji")] Adopcja adopcja)
+        public async Task<IActionResult> Create(
+            [Bind("IdAdopcji,IdZwierzecia,IdUzytkownika,IdPracownika,StatusAdopcji,DataRozpoczeciaAdopcji,DataZakonczeniaAdopcji")] Adopcja adopcja)
         {
             if (ModelState.IsValid)
             {
+                // Dla tworzenia można nadal użyć zwykłego EF
                 _context.Add(adopcja);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+
             ViewData["IdPracownika"] = new SelectList(_context.Pracownik, "IdPracownika", "IdPracownika", adopcja.IdPracownika);
             ViewData["IdUzytkownika"] = new SelectList(_context.Uzytkownik, "IdUzytkownika", "IdUzytkownika", adopcja.IdUzytkownika);
             ViewData["IdZwierzecia"] = new SelectList(_context.Zwierze, "IdZwierzecia", "IdZwierzecia", adopcja.IdZwierzecia);
             return View(adopcja);
         }
+
         [Authorize(Roles = "Admin")]
         // GET: Adopcjas/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var adopcja = await _context.Adopcja.FindAsync(id);
-            if (adopcja == null)
-            {
-                return NotFound();
-            }
+            if (adopcja == null) return NotFound();
+
             ViewData["IdPracownika"] = new SelectList(_context.Pracownik, "IdPracownika", "IdPracownika", adopcja.IdPracownika);
             ViewData["IdUzytkownika"] = new SelectList(_context.Uzytkownik, "IdUzytkownika", "IdUzytkownika", adopcja.IdUzytkownika);
             ViewData["IdZwierzecia"] = new SelectList(_context.Zwierze, "IdZwierzecia", "IdZwierzecia", adopcja.IdZwierzecia);
+
             return View(adopcja);
         }
 
         // POST: Adopcjas/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdAdopcji,IdZwierzecia,IdUzytkownika,IdPracownika,StatusAdopcji,DataRozpoczeciaAdopcji,DataZakonczeniaAdopcji")] Adopcja adopcja)
+        public async Task<IActionResult> Edit(
+            int id,
+            [Bind("IdAdopcji,IdZwierzecia,IdUzytkownika,IdPracownika,StatusAdopcji,DataRozpoczeciaAdopcji,DataZakonczeniaAdopcji")] Adopcja adopcja)
         {
             if (id != adopcja.IdAdopcji)
             {
@@ -111,45 +107,59 @@ namespace ProjektC.Controllers
             {
                 try
                 {
-                    _context.Update(adopcja);
-                    await _context.SaveChangesAsync();
+                    // --------------- ZAMIANA standardowego UPDATE na procedurę ---------------
+                    // Wywołujemy w bazie spUpdateAdopcja (musi istnieć).
+                    // Parametry: (id_adopcji, data_rozp..., data_zakonczenia..., id_prac, id_uzytk, id_zwierz)
+                    await _context.Database.ExecuteSqlRawAsync(
+                        @"EXEC spUpdateAdopcja 
+                           @p0, 
+                           @p1, 
+                           @p2, 
+                           @p3, 
+                           @p4, 
+                           @p5",
+                        adopcja.IdAdopcji,
+                        adopcja.DataRozpoczeciaAdopcji,
+                        adopcja.DataZakonczeniaAdopcji,  // może być null
+                        adopcja.IdPracownika,
+                        adopcja.IdUzytkownika,
+                        adopcja.IdZwierzecia
+                    );
+
+                    // Nie wywołujemy już _context.Update(adopcja) i SaveChanges() dla Adopcji,
+                    // bo to spowodowałoby 'UPDATE ... OUTPUT ...' i błąd z triggerem.
+                    // Teraz cała logika update + wpis do logu jest w spUpdateAdopcja.
+
+                    return RedirectToAction(nameof(Index));
                 }
                 catch (DbUpdateConcurrencyException)
                 {
                     if (!AdopcjaExists(adopcja.IdAdopcji))
-                    {
                         return NotFound();
-                    }
                     else
-                    {
                         throw;
-                    }
                 }
-                return RedirectToAction(nameof(Index));
             }
+
+            // Walidacja się nie powiodła -> wracamy do widoku
             ViewData["IdPracownika"] = new SelectList(_context.Pracownik, "IdPracownika", "IdPracownika", adopcja.IdPracownika);
             ViewData["IdUzytkownika"] = new SelectList(_context.Uzytkownik, "IdUzytkownika", "IdUzytkownika", adopcja.IdUzytkownika);
             ViewData["IdZwierzecia"] = new SelectList(_context.Zwierze, "IdZwierzecia", "IdZwierzecia", adopcja.IdZwierzecia);
             return View(adopcja);
         }
+
         [Authorize(Roles = "Admin")]
         // GET: Adopcjas/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var adopcja = await _context.Adopcja
                 .Include(a => a.IdPracownikaNavigation)
                 .Include(a => a.IdUzytkownikaNavigation)
                 .Include(a => a.IdZwierzeciaNavigation)
                 .FirstOrDefaultAsync(m => m.IdAdopcji == id);
-            if (adopcja == null)
-            {
-                return NotFound();
-            }
+            if (adopcja == null) return NotFound();
 
             return View(adopcja);
         }
@@ -164,7 +174,6 @@ namespace ProjektC.Controllers
             {
                 _context.Adopcja.Remove(adopcja);
             }
-
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
